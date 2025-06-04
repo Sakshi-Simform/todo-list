3d4db857a296196ccb83a0d35c2864d674d2af5a
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-04 14:06:13 +0530

    feature: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Provider } from 'react-redux';
 import TodoWrapper from './components/TodoWrapper';
+import { store } from './components/store/store';
 import './styles/App.css'
+ import './styles/Theme.css'
 
 function App() {
   return (
+    <Provider store={store}>
    <TodoWrapper />
+   </Provider>
   )
 }
 export default App;
