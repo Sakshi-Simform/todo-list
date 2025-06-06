04f4e249ad5a9615ed304ad3aaf65a8113dda0ec
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-06 10:25:04 +0530

    feature: change indentation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Provider } from 'react-redux';
 import TodoWrapper from '@/components/TodoWrapper';
 import { store } from '@/store/store';
 import '@/styles/App.css'
- import '@/styles/Theme.css'
+import '@/styles/Theme.css'
 
 function App() {
   return (
     <Provider store={store}>
-   <TodoWrapper />
-   </Provider>
+      <TodoWrapper />
+    </Provider>
   )
 }
 export default App;
